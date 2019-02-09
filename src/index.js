a749141ec99eed15ae7ed78e85bcdc477b990ca0
marcgregi <marc@hypedigital.co>
2019-02-09 22:47:19 +0000

    Creating Robot friends dynamic cards using react JSX.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CardList from './CardList'; 
+import * as serviceWorker from './serviceWorker';
+import 'tachyons'; 
+import {robots} from './robots'; 
+
+ReactDOM.render(
+            <CardList />
+, document.getElementById('root'));
+
+
+serviceWorker.unregister();
+ 
