d30e7f9c7c72565de5bf69ead9f47c1206921651
marcgregi <marc@hypedigital.co>
2019-02-11 20:33:39 +0000

    Changing robots array to JSON Placeholder temp server which contains users json.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'; 
 import CardList from './CardList'; 
 import SearchBox from './SearchBox'; 
-import { robots } from './robots'; 
 import './app.css'; 
 
 
@@ -11,9 +10,20 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots, 
+            robots: [], 
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+          return  response.json(); 
+        })
+        .then(users => {
+            this.setState({ robots: users}); 
+        })
+        
     }
 
     onSearchChange = (event) => {
@@ -26,15 +36,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
-            return (
-                <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} /> 
-                <CardList robots={filteredRobots}/>
-                </div>
-            
-        ); 
+            if (this.state.robots.length === 0) {
+                return <h1>Loading...</h1>
+            } else {
+                return (
+                    <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} /> 
+                    <CardList robots={filteredRobots}/>
+                    </div>
+                
+                    ); 
 
+            }
+           
     }
     
 }
